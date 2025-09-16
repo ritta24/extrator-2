cfcb638870223b44ada82290a64d13895a656f54
ritta24 <rita.thainara.paula@escola.pr.gov.br>
2025-09-16 09:32:39 -0300

    aula 4

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChave );
 
 function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
-
     const campoResultado = document.querySelector("#resultado-palavrachave");
-
-    const palavras = texto.split(" ");
+    const palavrasChave = processaTexto(texto);
 
     campoResultado.textContent = palavras;
+}
+
+function processaTexto(texto) {
+    let palavras = texto.split(/\P[L]+/u);
+    return palavras;
 }
