a2eed5cdcc0fd3ce5fb20e5d5ccf3b696ea21064
ritta24 <rita.thainara.paula@escola.pr.gov.br>
2025-09-23 09:40:31 -0300

    terminado aula 5

--- script.js
+++ script.js
@@ -6,11 +6,22 @@ function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
     const campoResultado = document.querySelector("#resultado-palavrachave");
     const palavrasChave = processaTexto(texto);
-
-    campoResultado.textContent = palavras;
 }
 
-function processaTexto(texto) {
+    campoResultado.textContent = palavras;
+
+    function processaTexto(texto) {
     let palavras = texto.split(/\P[L]+/u);
+
+let frequencias = ();
+for (let i of palavras) {
+     frequencia{i} =0;
+        for(let j of palavras) {
+            if (i==j) {
+                 frequencia(i)++;
+                }
+             }
+
+}
     return palavras;
 }
